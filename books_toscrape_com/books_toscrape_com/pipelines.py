5ad7e67852e45bc50e5fa62175b2bf9849d880de
JDTheRipperPC <jdtheripperpc@gmail.com>
2019-07-22 22:50:24 +0200

    pipeline test

--- books_toscrape_com/books_toscrape_com/pipelines.py
+++ books_toscrape_com/books_toscrape_com/pipelines.py
@@ -6,6 +6,30 @@
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
 
+class JsonWritePipeline(object):
+
+    def __init__(self, filedir, filename):
+        self.filedir = filedir
+        self.filename = filename
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        import pdb; pdb.set_trace()
+        return cls(
+            filedir=crawler.settings.get('JSON_FILEDIR'),
+            filename=crawler.settings.get('JSON_FILENAME')
+        )
+
+    def open_spider(self, spider):
+        pass
+
+    def close_spider(self, spider):
+        pass
+
+    def process_item(self, item, spider):
+        pass
+
+
 class BooksToscrapeComPipeline(object):
     def process_item(self, item, spider):
         return item
