83bd357da119a84c8f207dda899e73b1d4cc4f25
JDTheRipperPC <jdtheripperpc@gmail.com>
2019-07-27 16:36:55 +0200

    Fixed custom CSV item exporter

--- books_toscrape_com/books_toscrape_com/exporters.py
+++ books_toscrape_com/books_toscrape_com/exporters.py
@@ -1,16 +1,19 @@
+import csv
+
 from scrapy.utils.project import get_project_settings
 from scrapy.exporters import CsvItemExporter
 
 
 class CsvExtendedItemExporter(CsvItemExporter):
 
-    def __init__(self, file, **kwargs):
+    def __init__(self, *args, **kwargs):
         settings = get_project_settings()
-        include_headers_line = settings.get('CSV_INCLUDE_HEADER_LINE', True)
-        join_multivalued = settings.get('CSV_DELIMITER', ',')
-        super(CsvExtendedItemExporter, self).__init__(
-            file,
-            include_headers_line=include_headers_line,
-            join_multivalued=join_multivalues,
-            **kwargs
-        )
+        dialect = settings.get('CSV_DIALECT')
+        kwargs['dialect'] = csv.get_dialect('dialect') if dialect in csv.list_dialects() else csv.get_dialect('excel')
+        kwargs['delimiter'] = settings.get('CSV_DELIMITER', kwargs['dialect'].delimiter)
+        kwargs['doublequote'] = settings.get('CSV_DOUBLEQUOTE', kwargs['dialect'].doublequote)
+        kwargs['escapechar'] = settings.get('CSV_ESCAPECHAR', kwargs['dialect'].escapechar)
+        kwargs['lineterminator'] = settings.get('CSV_LINETERMINATOR', kwargs['dialect'].lineterminator)
+        kwargs['quotechar'] = settings.get('CSV_QUOTECHAR', kwargs['dialect'].quotechar)
+        kwargs['quoting'] = settings.get('CSV_QUOTING', kwargs['dialect'].quoting)
+        super(CsvExtendedItemExporter, self).__init__(*args, **kwargs)
